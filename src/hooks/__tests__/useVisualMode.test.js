7f4a3e4b9a34528d92f5bb8edefa191b59c2c3ad
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-01 17:22:26 -0700

    add transition, second test passing

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,5 +1,5 @@
 // src/hooks/__tests__/useVisualMode.test
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
@@ -8,5 +8,13 @@ test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
+});
+
+const SECOND = "SECOND";
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
 
