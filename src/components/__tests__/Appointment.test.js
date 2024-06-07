f9bf869a6e4ff0b8ee49cc7894de2d9d44d9aeed
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-07 15:24:15 -0700

    Add tests for Form that check if the component renders without a student name and if the component renders with a student name

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,6 +1,8 @@
 // Appointment.test.js
 import React from "react";
+
 import { render, cleanup } from "@testing-library/react";
+
 import Appointment from "components/Appointment";
 
 afterEach(cleanup);
