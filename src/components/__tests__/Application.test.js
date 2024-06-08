b288c3e344aacc01ec8e0f55cb31b136a5a82aa8
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-07 17:00:04 -0700

    add a test to Application defaults to Monday and changes the schedule when a new day is selected

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
